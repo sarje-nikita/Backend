ab6619b692574537aa3493caf3bfd8fcffd8c55c
sarje-nikita <contact.nikitasarje@gmail.com>
2024-11-24 09:54:24 +0530

    initaliza express and add custome Api error and repose class

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,9 @@ const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MOGODB_URI}/${DB_NAME}`
+
     );
+    console.log("database connected to the following  host : ")
     console.log(connectionInstance.connection.host);
   } catch (error) {
     console.error("MONGODB connection error", error);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`app listning on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("database connection error");
+  });
